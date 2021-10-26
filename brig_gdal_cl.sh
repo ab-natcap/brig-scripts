0ff6cc789a042e35be8c046ce27714a4ef359e56
Allison Bailey <allison.bailey@stanford.edu>
2021-10-26 15:08:09 -0700

    Added GDAL commands for WorldPop

--- brig_gdal_cl.sh
+++ brig_gdal_cl.sh
@@ -1,5 +1,6 @@
 # GDAL and shell commands for preprocessing of BRIG data sets
 
+
 # Bathymetry
 # source data, GEBCO 2014
 #  https://www.gebco.net/data_and_products/historical_data_sets/#gebco_2014
@@ -28,3 +29,37 @@ Mask the HGT raster to include only land elevations.
 gdal_calc.py -A NASADEM_NC.001_NASADEM_HGT_doy2000042_aid0001.tif -B NASADEM_NUMNC.001_NASADEM_NUM_doy2000042_aid0001.tif --co TILED=YES --co COMPRESS=LZW --overwrite --outfile NASADEM_HGT_landonly.tif --calc="(B>0)*A + (B==0)*(-32767)"
 
 gdalwarp -overwrite -r bilinear -t_srs EPSG:32618 -tr 30 30 -tap -cutline ../../work/bhs_region.shp -crop_to_cutline NASADEM_HGT_landonly.tif ../../work/bathytopo/nasadem001land_bahamas_32618.tif
+
+
+
+# WorldPop
+cd '/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/pop'
+# change no data values to zeros
+gdal_calc.py -A ../../source/worldpop/bhs_ppp_2020_UNadj_constrained.tif --outfile=intermediate.tif --calc="A" --NoDataValue=0
+# Reassign -99999 as no data value
+gdal_translate intermediate.tif bhs_ppp_2020_UNadj_constrained_ndto0.vrt -a_nodata -99999
+# Mask the constrained with unconstrained to get zeros on land and no data in the water
+gdal_calc.py --overwrite -A bhs_ppp_2020_UNadj_constrained_ndto0.vrt -B ../../source/worldpop/bhs_ppp_2020_UNadj.tif --outfile bhs_ppp_2020_UNadj_constrained_zeros.tif --calc="A*(B>0)" --NoDataValue=-99999
+
+# Reproject rasters to UTM-18N, using nearest neighbor
+cd '/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/pop'
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW ../../source/worldpop/bhs_ppp_2020_UNadj.tif bhs_ppp_2020_UNadj_32618.tif
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW ../../source/worldpop/bhs_ppp_2020_UNadj_constrained.tif bhs_ppp_2020_UNadj_constrained_32618.tif
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW bhs_ppp_2020_UNadj_constrained_zeros.tif bhs_ppp_2020_UNadj_constrained_zeros_32618.tif
+cd /Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/pop
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW ../../source/worldpop/bhs_ppp_2010_UNadj.tif bhs_ppp_2010_UNadj_32618.tif
+
+# Then, clip values to include only area within 2km of shoreline as in previous analysis python notebook
+cd /Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/pop
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -co TILED=YES -co COMPRESS=LZW \
+  bhs_ppp_2020_UNadj_32618.tif bhs_ppp_2020_UNadj_2kmbuff_32618.tif
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -co TILED=YES -co COMPRESS=LZW \
+  bhs_ppp_2020_UNadj_constrained_32618.tif bhs_ppp_2020_UNadj_constrained_2kmbuff_32618.tif
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -co TILED=YES -co COMPRESS=LZW \
+  bhs_ppp_2020_UNadj_constrained_zeros_32618.tif bhs_ppp_2020_UNadj_constrained_zeros_2kmbuff_32618.tif
---
+++ fbd4gpop.sh
@@ -0,0 +1,97 @@
+# fbd4gpop.sh
+# GDAL commands for pre-processing Facebook data for Good Bahamas Population Data
+#	Convert No Data pixels on land to zeros, and retain other no data pixels
+#	Project to EPSG 32618 (UTM18N)
+
+cd "/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/pop"
+
+
+### - General Population
+# change no data pixels to zeros
+# Source NoData = nan
+gdal_calc.py -A ../../source/hdx/bhs_general_2020.tif --outfile=nanto0.tif --calc="nan_to_num(A,nan=0.0)" --NoDataValue=-99999 --overwrite 
+
+# Mask raster to include only pixels on land (touched by shoreline poly).  All other will become No DAta
+gdalwarp -overwrite \
+  -cutline "/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/shoreline/bhs_shore.gpkg" \
+  -cl tnc_coastlinePlanet_2020 \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  nanto0.tif \
+  intermediate.tif
+# Then unset No Data so all water areas are value -99999
+gdal_edit.py -unsetnodata -unsetstats intermediate.tif
+
+# The shoreline does not touch all pixels with a population value, so paste them back in from the unmasked image
+gdal_calc.py --overwrite -A intermediate.tif -B nanto0.tif --outfile fbd4g_bhs_general_2020_zeros.tif --calc="B*logical_and(A==-99999,B>0) + A*logical_and(A==-99999,B==0) + A*(A>=0)" --NoDataValue=-99999
+
+
+
+### - Children under 5
+# change no data pixels to zeros
+# Source NoData = nan
+gdal_calc.py -A ../../source/hdx/bhs_children_under_five_2020.tif --outfile=nanto0.tif --calc="nan_to_num(A,nan=0.0)" --NoDataValue=-99999 --overwrite
+
+# Mask raster to include only pixels on land.  All other will become No DAta
+gdalwarp -overwrite \
+  -cutline "/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/shoreline/bhs_shore.gpkg" \
+  -cl tnc_coastlinePlanet_2020 \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  nanto0.tif \
+  intermediate.tif
+# Then unset No Data so all water areas are value -99999
+gdal_edit.py -unsetnodata -unsetstats intermediate.tif
+
+# The shoreline does not touch all pixels with a population value, so paste them back in from the unmasked image
+gdal_calc.py --overwrite -A intermediate.tif -B nanto0.tif --outfile fbd4g_bhs_children_under_five_2020_zeros.tif --calc="B*logical_and(A==-99999,B>0) + A*logical_and(A==-99999,B==0) + A*(A>=0)" --NoDataValue=-99999
+
+
+
+### - Elderly 60 plus
+# change no data pixels to zeros
+# Source NoData = nan
+gdal_calc.py -A ../../source/hdx/bhs_elderly_60_plus_2020.tif --outfile=nanto0.tif --calc="nan_to_num(A,nan=0.0)" --NoDataValue=-99999 --overwrite
+
+# Mask raster to include only pixels on land.  All other will become No DAta
+gdalwarp -overwrite \
+  -cutline "/Users/arbailey/Google Drive/Shared drives/brigBH2021/data/work/shoreline/bhs_shore.gpkg" \
+  -cl tnc_coastlinePlanet_2020 \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  nanto0.tif \
+  intermediate.tif
+# Then unset No Data so all water areas are value -99999
+gdal_edit.py -unsetnodata -unsetstats intermediate.tif
+
+# The shoreline does not touch all pixels with a population value, so paste them back in from the unmasked image
+gdal_calc.py --overwrite -A intermediate.tif -B nanto0.tif --outfile fbd4g_bhs_elderly_60_plus_2020_zeros.tif --calc="B*logical_and(A==-99999,B>0) + A*logical_and(A==-99999,B==0) + A*(A>=0)" --NoDataValue=-99999
+
+
+## Delete intermediate files
+rm nanto0.tif
+rm intermediate.tif
+
+
+## Reproject to EPSG:32618 (UTM18N, WGS-84)
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW fbd4g_bhs_general_2020_zeros.tif fbd4g_bhs_general_2020_zeros_326818.tif
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW fbd4g_bhs_children_under_five_2020_zeros.tif fbd4g_bhs_children_under_five_2020_zeros_326818.tif
+gdalwarp -overwrite -r near -t_srs EPSG:32618 -co TILED=YES -co COMPRESS=LZW fbd4g_bhs_elderly_60_plus_2020_zeros.tif fbd4g_bhs_elderly_60_plus_2020_zeros_326818.tif
+
+
+## Clip raster to include only area within 2km of shoreline as in 2017 analysis python notebook
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  -co TILED=YES -co COMPRESS=LZW \
+  fbd4g_bhs_general_2020_zeros_326818.tif fbd4g_bhs_general_2020_zeros_2kmbuff_32618.tif
+
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  -co TILED=YES -co COMPRESS=LZW \
+  fbd4g_bhs_children_under_five_2020_zeros_326818.tif fbd4g_bhs_children_under_five_2020_zeros_2kmbuff_32618.tif
+
+gdalwarp -overwrite \
+  -cutline ../shoreline/coastline_extract_2km_buffer.shp \
+  -wo CUTLINE_ALL_TOUCHED=TRUE \
+  -co TILED=YES -co COMPRESS=LZW \
+  fbd4g_bhs_elderly_60_plus_2020_zeros_326818.tif fbd4g_bhs_elderly_60_plus_2020_zeros_2kmbuff_32618.tif
+ 
